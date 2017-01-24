10aa496f638bebca3d8e526862400c5061d59831
nirmal_artifici <nirmal@artifici.com>
2017-01-24 13:31:15 +0530

    trellio_admin and related changes were missing

--- trellio/__init__.py
+++ trellio/__init__.py
@@ -14,5 +14,7 @@ from .services import (get, post, head, put, patch, delete, options, trace)  # n
 from .utils import log  # noqa
 from .utils.log import setup_logging  # noqa
 from .wrappers import Response, Request  # noqa
+from .conf_manager.conf_client import ConfigHandler, InvalidConfigurationError
+from .management.commands import ManagementRegistry, ManagementCommand, InvalidCMDArguments
 
 __version__ = '1.1.23b'
